1bac99cc17b1e99dea3194a08d72cc339c7b4964
Zoltan102 <suveges.zoltan@petrik.hu>
2022-09-21 14:04:10 +0200

    nav

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import './main.css';
+
+function init(){
+
+}
+
+document.addEventListener('DOMContentLoaded', init());
